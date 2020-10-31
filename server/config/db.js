dc07bdde9907f52b2ad76d664ba026354345fc6e
ysf1472 <64108009+ysf1472@users.noreply.github.com>
2020-10-30 21:41:45 -0400

    Final Adjustments for add, delete, and edit buttuns

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Yusuf2:bpC0rO3H9XJSZJ88@mongodbserver2.nzkad.mongodb.net/Midterm-books229?retryWrites=true&w=majority"
+  // "URI": "mongodb://localhost/books229"
+};
